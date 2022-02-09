92bc893d1b99d8ec211edffc434ca8546d8dd346
Alberto Arias <alberto@grid.ai>
2022-02-09 19:12:17 +0100

    Add logic to support two way communication

--- src/components/IFrameRoute.tsx
+++ src/components/IFrameRoute.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { styled } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 import useLightningState from "hooks/useLightningState";
 
@@ -8,31 +8,57 @@ type Props = {
   iframeTargetUrl: string;
 };
 
-const IFrame = styled("iframe")({
-  height: "100%",
-  width: "100%",
-});
+const isRegisterPortMessage = (
+  message: MessageEvent,
+  iframeTargetUrl: string
+) => {
+  return (
+    (message.data === "Stablish communication") &&
+    (message.origin === iframeTargetUrl) &&
+    message?.ports?.[0]
+  );
+};
 
 export default function IFrameRoute(props: Props) {
-  let [loaded, setLoaded] = useState(false);
+  const [iframeState, setIframeState] = useState("No state reported yet");
+  const [iframePort, setIframePort] = useState<MessagePort | undefined>(undefined);
   const iframeRef = useRef<HTMLIFrameElement>(null);
-
-  const onLoadHandler = () => setLoaded(true);
   const lightningState = useLightningState();
 
   useEffect(() => {
-    if (loaded && lightningState.data) {
-      iframeRef.current?.contentWindow?.postMessage(lightningState.data, props.iframeTargetUrl);
+    window.addEventListener('message', (message: MessageEvent) => {
+      if(isRegisterPortMessage(message, props.iframeTargetUrl)) {
+        const internalPort = message.ports[0];
+        message.ports[0].onmessage = (message: MessageEvent) => {
+          if (message.data === "Subscribed" && lightningState.data) {
+            internalPort.postMessage(lightningState.data);
+          } else {
+            setIframeState(message.data);
+          }
+        };
+        setIframePort(message.ports[0]);
+      }
+    });
+  }, [iframePort]); 
+
+  useEffect(() => {
+    if (iframePort && lightningState.data) {
+      iframePort.postMessage(lightningState.data);
     }
-  });
+  }, [lightningState.data]);
 
   return (
-    <IFrame 
-      name={props.name} 
-      src={props.iframeTargetUrl} 
-      title={props.name} 
-      ref={iframeRef}
-      onLoad={onLoadHandler}
-    />
+    <Box display={"flex"} flexDirection={"column"} height={"100%"}>
+      <Typography>Component State: {iframeState}</Typography>
+      <Box
+        height={"100%"}
+        width={"100%"}
+        component={"iframe"}
+        name={props.name} 
+        src={props.iframeTargetUrl} 
+        title={props.name} 
+        ref={iframeRef}
+      />
+    </Box>
   );
 }
