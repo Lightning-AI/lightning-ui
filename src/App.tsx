8ef97d1376b37efc7cf738845eb4299bf6d1e00b
Alberto Arias <alberto@grid.ai>
2022-02-24 19:11:24 +0100

    Restore App file content

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { Typography } from "@mui/material";
-import AcUnitIcon from '@mui/icons-material/AcUnit';
 import React from "react";
-// import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-// import AppRoutes from "components/AppRoutes";
+import AppRoutes from "components/AppRoutes";
 
 function App() {
   return (
-    <>
-      <AcUnitIcon />
-      <Typography fontFamily={"UCity"} fontWeight={300}>UCity Font Family 300</Typography>
-      <Typography fontFamily={"UCity"} fontWeight={400}>UCity Font Family 400</Typography>
-      <Typography fontFamily={"UCity"} fontWeight={600}>UCity Font Family 600</Typography>
-      <Typography fontFamily={"Roboto"} fontWeight={400}>Roboto Font Family 400</Typography>
-    </>
+    <Router>
+      <AppRoutes />
+    </Router>
   );
 }
 
