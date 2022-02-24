d2b72208c0c0f5b85165e4d9f4de5a2e66db9787
Alberto Arias <alberto@grid.ai>
2022-02-24 18:50:19 +0100

    Add theme to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,14 @@
-export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+import React from 'react';
+import ThemeProvider, { theme } from '../src/design-system/theme';
+import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
+// import '../src/design-system/style.css';
+
+export const decorators = [
+  (Story) => (
+      <ThemeProvider>
+          <EmotionThemeProvider theme={theme}>
+              {Story()}
+          </EmotionThemeProvider>
+      </ThemeProvider>
+  )
+];
