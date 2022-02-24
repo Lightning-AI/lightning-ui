011636dc82ca6ff95d43c60c8e2b59d87575d8cd
Alberto Arias <alberto@grid.ai>
2022-02-24 18:52:38 +0100

    Add theme propagation fix explanation

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ThemeProvider, { theme } from '../src/design-system/theme';
+// Required to get theme propagated in storybook see https://github.com/mui/material-ui/issues/24282#issuecomment-952211989
 import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
-// import '../src/design-system/style.css';
 
 export const decorators = [
   (Story) => (
