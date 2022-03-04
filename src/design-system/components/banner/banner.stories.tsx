17f083e3cb4702eada8d88ccdbe34ced4fb7b3e5
Alberto Arias <alberto@grid.ai>
2022-03-04 15:12:46 +0100

    Fix figma url

--- src/design-system/components/banner/banner.stories.tsx
+++ src/design-system/components/banner/banner.stories.tsx
@@ -8,7 +8,7 @@ export default {
   parameters: {
     design: {
       type: "figma",
-      url: "https://www.figma.com/file/tLrz5T82EKYMnqgvnk1Ldo/UILibrary?node-id=31299%3A63735",
+      url: "https://www.figma.com/file/tLrz5T82EKYMnqgvnk1Ldo/UILibrary?node-id=33152%3A71640",
     },
   },
   argTypes: {
