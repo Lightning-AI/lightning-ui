7153d2cdb7f44c4b0cbba2c29e4ca5be4c7020c7
Alberto Arias <alberto@grid.ai>
2022-03-15 20:15:43 +0100

    Include components to index

--- src/design-system/components/index.tsx
+++ src/design-system/components/index.tsx
@@ -1,7 +1,9 @@
 import Typography from "./typography";
 import Button from "./button";
 import Label from "./label";
+import { Dialog, DialogTitle, DialogContent, DialogActions } from "./dialog";
+import Popover from "./popover";
 
 export * from "@mui/material";
 
-export { Typography, Button, Label };
+export { Typography, Button, Label, Dialog, DialogTitle, DialogContent, DialogActions, Popover };
