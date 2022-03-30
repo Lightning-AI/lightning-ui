e4fcc9c8bcd8b1365ccfab7961193933f098b451
Alberto Arias <alberto@grid.ai>
2022-03-30 20:45:21 +0200

    It should be ReactNode type

--- src/design-system/components/media/index.tsx
+++ src/design-system/components/media/index.tsx
@@ -5,7 +5,7 @@ export type MediaProps = {
   variant?: "square" | "circle" | "portrait" | "landscape";
   size?: 16 | 24 | 32 | 48 | 64 | 96 | 144 | 180;
   src?: string;
-  fallbackIcon?: React.ReactElement;
+  fallbackIcon?: React.ReactNode;
 };
 
 const Media = ({ variant = "square", size = 16, src, fallbackIcon }: MediaProps) => {
