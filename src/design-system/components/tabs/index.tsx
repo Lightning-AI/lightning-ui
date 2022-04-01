8234c0106dcefe8aa71bbe20743050a49e49ee1c
Alberto Arias <alberto@grid.ai>
2022-04-01 14:33:45 +0200

    Add navigable tabs logic

--- src/design-system/components/tabs/index.tsx
+++ src/design-system/components/tabs/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useState } from "react";
+import { ReactNode, MouseEventHandler, useState, useEffect } from "react";
 
 import { Box, Divider, SxProps, Theme } from "../";
 import MuiTab from "@mui/material/Tab";
@@ -6,23 +6,51 @@ import MuiTabs from "@mui/material/Tabs";
 
 import TabPanel from "./TabPanel";
 import TabContent from "./TabContent";
-import { SystemStyleObject } from "@mui/system";
+import { useNavigate, useLocation } from "react-router-dom";
 
-export type TabItem = {
-  content: ReactElement;
+type StaticTabItem = {
   title: string;
+  content: ReactNode;
 };
+
+type NavigableTabItem = {
+  title: string;
+  href: string;
+  content?: ReactNode;
+};
+
+export type TabItem = StaticTabItem | NavigableTabItem;
 
 export type TabsProps = {
   selectedTab?: number;
   tabItems: TabItem[];
   variant?: "text" | "outlined";
-  sxTabs: SxProps<Theme>;
-  sxContent: SxProps<Theme>;
+  sxTabs?: SxProps<Theme>;
+  sxContent?: SxProps<Theme>;
 };
 
 const Tabs = (props: TabsProps) => {
   const [selectedTab, setSelectedTab] = useState(props.selectedTab || 0);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const navigateHandler = (url: string, value: number): MouseEventHandler<HTMLButtonElement> => {
+    return event => {
+      event.preventDefault();
+      setSelectedTab(value);
+      navigate(url);
+    };
+  };
+
+  const hasContent = props.tabItems.some((tabItem: any) => typeof tabItem.content !== "undefined");
+  const locationUri = location.pathname + location.hash + location.search;
+
+  useEffect(() => {
+    const hrefIndex = props.tabItems.findIndex((tabItem: any) => tabItem.href === locationUri);
+    const newSelectedTab = hrefIndex !== -1 ? hrefIndex : 0;
+    setSelectedTab(newSelectedTab);
+  }, [location]);
 
   return (
     <Box>
@@ -31,19 +59,26 @@ const Tabs = (props: TabsProps) => {
         onChange={(e, value) => setSelectedTab(value)}
         variant={"scrollable"}
         sx={props.sxTabs}>
-        {props.tabItems.map(tabItem => (
+        {props.tabItems.map((tabItem: any, index) => (
           // @ts-ignore
-          <MuiTab key={tabItem.title} label={tabItem.title} variant={props.variant} />
+          <MuiTab
+            key={tabItem.title}
+            label={tabItem.title}
+            variant={props.variant}
+            onClick={tabItem.href && navigateHandler(tabItem.href, index)}
+          />
         ))}
       </MuiTabs>
-      <Divider/>
-      <Box paddingY={1.5} sx={props.sxContent}>
-        {props.tabItems.map((tabItem, index) => (
-          <TabPanel key={index.toString()} value={selectedTab} index={index}>
-            <TabContent>{tabItem.content}</TabContent>
-          </TabPanel>
-        ))}
-      </Box>
+      <Divider />
+      {hasContent && (
+        <Box paddingY={1.5} sx={props.sxContent}>
+          {props.tabItems.map((tabItem: any, index) => (
+            <TabPanel key={index.toString()} value={selectedTab} index={index}>
+              <TabContent>{tabItem.content}</TabContent>
+            </TabPanel>
+          ))}
+        </Box>
+      )}
     </Box>
   );
 };
