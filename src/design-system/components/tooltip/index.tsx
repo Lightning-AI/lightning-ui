42603f2ba0bcf5046ab8004df3d78d9ce82087db
Yurij Mikhalevich <yurij@grid.ai>
2022-05-10 17:55:24 +0400

    cleanup

--- src/design-system/components/tooltip/index.tsx
+++ src/design-system/components/tooltip/index.tsx
@@ -1,6 +1,6 @@
 import MuiTooltip, { TooltipProps as MuiTooltipProps } from "@mui/material/Tooltip";
 
-export type TooltipProps = Pick<MuiTooltipProps, "className" | "classes" | "sx" | "title" | "children" | "placement">;
+export type TooltipProps = Pick<MuiTooltipProps, "className" | "classes" | "title" | "children" | "placement">;
 
 const Tooltip = (props: TooltipProps) => {
   return <MuiTooltip {...props} />;
