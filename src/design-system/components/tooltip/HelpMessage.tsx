421596b8334f2504438db16c72a51a3b4fb2ecce
Yurij Mikhalevich <yurij@grid.ai>
2022-05-11 13:37:57 +0400

    Update src/design-system/components/tooltip/HelpMessage.tsx
    Co-authored-by: Alberto Arias <alberto@grid.ai>

--- src/design-system/components/tooltip/HelpMessage.tsx
+++ src/design-system/components/tooltip/HelpMessage.tsx
@@ -10,7 +10,7 @@ export default function HelpMessage(props: TooltipProps) {
         tooltip: {
           sx: {
             maxWidth: 220,
-            backgroundColor: "#5B5E69",
+            backgroundColor: (theme) => theme.palette.grey[70],
           },
         },
       }}
