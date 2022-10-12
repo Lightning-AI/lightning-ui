37d8070bc2d757f0e5b28988fe078fcc2a6aa372
Alberto Arias <alberto@grid.ai>
2022-10-12 15:08:23 +0200

    First version Switch element

---
+++ src/design-system/components/switch/index.tsx
@@ -0,0 +1,37 @@
+import { ChangeEvent } from "react";
+
+import MuiSwitch, { SwitchProps as MuiSwitchProps } from "@mui/material/Switch";
+
+import { InfoIconWithHelpTooltip, Stack, Typography } from "..";
+import FormLabel from "../form-label";
+
+export type SwitchProps = Pick<MuiSwitchProps, "checked" | "disabled" | "size"> & {
+  label: string;
+  tooltip?: string;
+  onChange: (value: boolean) => void;
+};
+
+function Switch({ label, tooltip, checked, onChange, disabled, size }: SwitchProps) {
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.checked);
+  };
+  return (
+    <Stack direction={"row"} spacing={0.5} alignItems={"center"}>
+      <Typography
+        sx={{
+          color: (theme: any) => theme.palette.grey[disabled ? 40 : 100],
+          fontFamily: "Roboto",
+          fontWeight: 400,
+          fontStyle: "normal",
+          fontSize: "14px",
+          lineHeight: "20px",
+        }}>
+        {label}
+      </Typography>
+      {tooltip && <InfoIconWithHelpTooltip message={tooltip} size={"small"} />}
+      <MuiSwitch checked={checked} onChange={onChangeHandler} disabled={disabled} size={size} />
+    </Stack>
+  );
+}
+
+export default Switch;
---
+++ src/design-system/components/switch/switch.stories.tsx
@@ -0,0 +1,39 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import Switch, { SwitchProps } from ".";
+
+export default {
+  title: "Components/Switch",
+  component: Switch,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/tLrz5T82EKYMnqgvnk1Ldo/UILibrary?node-id=57007%3A76702",
+    },
+  },
+  args: {
+    label: "type of switch",
+    tooltip: "tooltip message",
+  },
+  argTypes: {
+    disabled: {
+      control: "boolean",
+      defaultValue: false,
+    },
+    checked: {
+      control: "boolean",
+      defaultValue: true,
+    },
+    size: {
+      control: "select",
+      options: [undefined, "small", "medium"],
+    },
+    onChange: { action: "switch Clicked!" },
+  },
+} as ComponentMeta<typeof Switch>;
+
+const Template: ComponentStory<any> = ({ ...args }: SwitchProps) => {
+  return <Switch {...args} />;
+};
+
+export const Playground = Template.bind({});
