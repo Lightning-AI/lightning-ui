75d54c28005a6cb1ff98659860da7506df0224a9
Adam Kuhn <adamjkuhn@gmail.com>
2023-02-09 14:29:06 -0500

    add “interactive” prop to tooltip
    * add interactive prop to allow cursor interaction with popover when appropriate
    * set default to disallow interaction to avoid UI overlap/interference

--- src/design-system/components/tooltip/index.tsx
+++ src/design-system/components/tooltip/index.tsx
@@ -5,11 +5,12 @@ import { Box } from "..";
 export type TooltipProps = Partial<Pick<MuiTooltipProps, "title">> &
   Pick<MuiTooltipProps, "children" | "placement"> & {
     width?: number | string;
+    interactive?: boolean;
   };
 
-const Tooltip = ({ title = "", children, placement = "top", width }: TooltipProps) => {
+const Tooltip = ({ title = "", children, placement = "top", width, interactive = true }: TooltipProps) => {
   return (
-    <MuiTooltip title={title} placement={placement}>
+    <MuiTooltip title={title} placement={placement} disableInteractive={interactive}>
       <Box component={"span"} sx={{ cursor: title ? "pointer" : "inherit", width }}>
         {children}
       </Box>
