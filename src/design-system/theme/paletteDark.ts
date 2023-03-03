723970ce289a813370a8053c2a59764ec754166d
William Falcon <waf2107@columbia.edu>
2023-03-03 07:35:44 -0500

    do or do not, there is no try - yoda

--- src/design-system/theme/paletteDark.ts
+++ src/design-system/theme/paletteDark.ts
@@ -4,7 +4,7 @@ import { ColorPartial, TypeBackground, TypeText } from "@mui/material/styles/cre
 const primary: PaletteColorOptions & Record<string, any> = {
   "main": "#792EE5",
   "70": "#AD00FF",
-  "50": "#E254FF",
+  "50": "#B45AFF",
   "40": "#AD8EEA",
   "20": "#8C35FF",
   "10": "#EFEEFF",
