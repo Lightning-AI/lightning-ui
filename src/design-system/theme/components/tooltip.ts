6f98f137bc3e38402e3c9c1fb8107f4f64df865b
William Falcon <waf2107@columbia.edu>
2023-03-05 10:03:15 -0500

    do or do not, there is no try - yoda

--- src/design-system/theme/components/tooltip.ts
+++ src/design-system/theme/components/tooltip.ts
@@ -10,6 +10,7 @@ const tooltip = {
           borderRadius: "8px",
           padding: "8px",
           maxWidth: "360px",
+          boxShadow: '0px 3px 5px -1px rgba(45, 64, 86, 0.2), 0px 6px 10px rgba(45, 64, 86, 0.14), 0px 1px 18px rgba(45, 64, 86, 0.12);',
           backgroundColor: theme.palette.primary[40],
           color: theme.palette.common.white,
           wordBreak: "normal",
