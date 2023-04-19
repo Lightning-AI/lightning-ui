14ed110f65ffdd679646043bae2c5906d2f7a3bf
Yurij Mikhalevich <yurij@lightning.ai>
2023-04-19 20:33:43 +0400

    improve wrapper

--- src/design-system/components/table/TableRowContentShowHover.tsx
+++ src/design-system/components/table/TableRowContentShowHover.tsx
@@ -2,10 +2,11 @@ import { Box } from "@mui/material";
 
 type TableRowContentShowOnHoverProps = {
   children: React.ReactNode;
+  showAlways?: boolean;
 };
 
 export const TableRowContentShowOnHoverClass = "LaiTableRowContentShowOnHover";
 
-export function TableRowContentShowOnHoverWrapper({ children }: TableRowContentShowOnHoverProps) {
-  return <Box className={TableRowContentShowOnHoverClass}>{children}</Box>;
+export function TableRowContentShowOnHoverWrapper({ children, showAlways }: TableRowContentShowOnHoverProps) {
+  return <Box display={"inline"} className={showAlways ? undefined : TableRowContentShowOnHoverClass}>{children}</Box>;
 }
